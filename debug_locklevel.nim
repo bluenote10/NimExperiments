52e533ad6f6ae7237e54c93d42ed230242965d84
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-02-18 23:05:19 +0100

    rearraged debug locklevel

--- debug_locklevel.nim
+++ debug_locklevel.nim
@@ -1,58 +1,67 @@
-import future
+when false:
+  import future
 
-type
-  DataFrame[T] = ref object of RootObj
+  when defined(nimlocks):
+    {.pragma: benign, gcsafe, locks: 0.}
+  else:
+    {.pragma: benign, gcsafe.}
 
-  CachedDataFrame[T] = ref object of DataFrame[T]
-    data: seq[T]
+  type
+    DataFrame[T] = ref object of RootObj
 
-  MappedDataFrame[T, U] = ref object of DataFrame[T]
-    orig: DataFrame[T]
-    mapper: proc(x: T): U
+    CachedDataFrame[T] = ref object of DataFrame[T]
+      data: seq[T]
 
-# -----------------------------------------------------------------------------
-# Iterators
-# -----------------------------------------------------------------------------
+    MappedDataFrame[T, U] = ref object of DataFrame[T]
+      orig: DataFrame[T]
+      mapper: proc(x: T): U {.locks: 0.}
 
-method iter[T](df: DataFrame[T]): (iterator(): T) {.base.} =
-  raise newException(IOError, "unimplemented")
+  # -----------------------------------------------------------------------------
+  # Iterators
+  # -----------------------------------------------------------------------------
 
-method iter[T](df: CachedDataFrame[T]): (iterator(): T) =
-  discard
+  method iter[T](df: DataFrame[T]): (iterator(): T) {.base, locks: 0.} =
+    raise newException(IOError, "unimplemented")
 
-method iter[T, U](df: MappedDataFrame[T, U]): (iterator(): U) =
-  discard
+  method iter[T](df: CachedDataFrame[T]): (iterator(): T) {.locks: 0.} =
+    discard
 
-# -----------------------------------------------------------------------------
-# Actions
-# -----------------------------------------------------------------------------
+  method iter[T, U](df: MappedDataFrame[T, U]): (iterator(): U) {.locks: 0.} =
+    discard
 
-method collect[T](df: DataFrame[T]): seq[T] {.base.} =
-  raise newException(IOError, "unimplemented")
+  # -----------------------------------------------------------------------------
+  # Actions
+  # -----------------------------------------------------------------------------
 
-method collect[T](df: CachedDataFrame[T]): seq[T] =
-  result = df.data
+  method collect[T](df: DataFrame[T]): seq[T] {.base, locks: nil.} =
+    raise newException(IOError, "unimplemented")
 
-method collect[S, T](df: MappedDataFrame[S, T]): seq[T] =
-  result = newSeq[T]()
-  let it = df.orig.iter()
-  for x in it():
-    result.add(df.mapper(x))
+  method collect[T](df: CachedDataFrame[T]): seq[T] {.locks: nil.} =
+    result = df.data
 
-# -----------------------------------------------------------------------------
-# Transformations
-# -----------------------------------------------------------------------------
+  method collect[S, T](df: MappedDataFrame[S, T]): seq[T] {.locks: nil.} =
+    result = newSeq[T]()
+    let it = df.orig.iter()
+    for x in it():
+      result.add(df.mapper(x))
 
-proc map[T, U](df: DataFrame[T], f: proc(x: T): U): DataFrame[U] =
-  result = MappedDataFrame[T, U](orig: df, mapper: f)
+  # -----------------------------------------------------------------------------
+  # Transformations
+  # -----------------------------------------------------------------------------
 
+  proc map[T, U](df: DataFrame[T], f: proc(x: T): U): DataFrame[U] =
+    result = MappedDataFrame[T, U](orig: df, mapper: f)
 
-let data = CachedDataFrame[int](data: @[1, 2, 3])
-echo data.map(x => x).collect()
+  let data = CachedDataFrame[int](data: @[1, 2, 3])
+  echo data.map(x => x).collect()
 
+else:
+  type SomeBase* = ref object of RootObj
+  type SomeDerived* = ref object of SomeBase
+    memberProc*: proc () # preferred solution: {.locks: 0.}
 
-
-
-
-
+  method testMethod(g: SomeBase) {.base, locks: nil.} = discard
+  method testMethod(g: SomeDerived) =
+    if g.memberProc != nil:
+      g.memberProc()
 
