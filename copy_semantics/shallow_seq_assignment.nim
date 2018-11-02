e0b6ad9bbc074aefef91bab3eb08e029244596f4
Fabian Keller <github.100.fkeller@spamgourmet.com>
2018-11-02 15:37:39 +0100

    shallow copy test

--- copy_semantics/shallow_seq_assignment.nim
+++ copy_semantics/shallow_seq_assignment.nim
@@ -1,3 +1,5 @@
+import strutils
+import strformat
 
 type
   DataUntyped* = ref object of RootObj
@@ -31,10 +33,59 @@ proc copyShallow[T](d: Data[T]): Data[T] =
   shallowCopy(result.data, d.data)
 
 
+proc convertToId[T](p: ptr T): string =
+  #[
+  echo p.repr
+  if p[].isNil:
+    "nil"
+  else:
+  ]#
+  (&"0x{cast[int](p[]):X}").toLowerAscii
+
+
+proc getDataId[T](d: Data[T]): string =
+  #let p = unsafeAddr(d.data)
+  #let id = "0x" & cast[int](p[]).toHex.toLowerAscii
+  #let id = (&"0x{cast[int](p[]):X}").toLowerAscii
+  #let id = "0x" & cast[int](d.data).toHex.toLowerAscii
+  #echo "d.repr: ", d.repr
+  #echo "p.repr: ", p.repr
+  #convertToId(p)
+  if d.data.len == 0:
+    "empty"
+  else:
+    let p = unsafeAddr(d.data)
+    convertToId(p)
+
+
+proc getId[T](d: Data[T]): string =
+  #let id = "0x" & cast[int](p[]).toHex.toLowerAscii
+  #let id = (&"0x{cast[int](p[]):X}").toLowerAscii
+  #let id = "0x" & cast[int](d.data).toHex.toLowerAscii
+  #echo "d.repr: ", d.repr
+  #echo "p.repr: ", p.repr
+  if d.isNil:
+    "nil"
+  else:
+    let p = unsafeAddr(d)
+    convertToId(p)
+
+
 let d1 = Data[int](data: @[1, 2, 3])
 let d2 = d1.copyShallow()
 
 echo d1[]
+echo d1.getId
+echo d1.getDataId
 echo d1.repr
+
 echo d2[]
+echo d2.getId
+echo d2.getDataId
 echo d2.repr
+
+var d3: Data[int]
+echo d3.getId
+
+var d4 = Data[int](data: @[])
+echo d4.getDataId
